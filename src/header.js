cd908d53ccccb508ba848efeda986f61fbbb0efd
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-06-28 18:25:29 +0200

    feat: main content created

---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import drawer from './images/drawers.jpg'
+
+const Header = () => {
+    return(
+        <>
+        <div>
+            <img src={drawer} alt="" class="illustration" />
+        </div>
+        </>
+    )
+}
+export default Header
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MainContent = () => {
+    return (
+        <div class='container'>
+            <h1>
+                Shift the overall look and feel by adding these wonderful
+                touches to furniture in your home
+            </h1>
+            <p>
+                Ever been in a room and felt like something was missing? Perhaps
+                it felt slightly bare and uninviting. I’ve got some simple tips
+                to help you make any room feel complete.
+            </p>
+            <footer>My footer</footer>
+        </div>
+    )
+}
+export default MainContent
